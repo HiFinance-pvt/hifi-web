d0f67a373f87163439fbfb4e28ff55d4993c1c31
root <root@LAPTOP-UNIRKUIJ.localdomain>
2025-07-21 21:58:12 +0000

    init repo

---
+++ src/app/(agents)/retirement-agent/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>Retirement Agent</div>
+  )
+}
+
+export default page
---
+++ src/app/(agents)/sebi-agent/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>SEBI Agent</div>
+  )
+}
+
+export default page
