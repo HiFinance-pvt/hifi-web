d98ac09822133588605d55a28ae077a40a8d4920
AdityaTote <adityatote23@gmail.com>
2025-07-26 16:08:34 +0530

    feat: zerodha redirect

---
+++ src/lib/api/kite..ts
@@ -0,0 +1,14 @@
+import { Axios } from "axios";
+
+export class Kite {
+  constructor(private readonly axios: Axios) {}
+
+  async redirect(request_token: string) {
+    const { data } = await this.axios.get("/kite/redirect", {
+      params: {
+        request_token: request_token
+      },
+    });
+    return data
+  }
+}
